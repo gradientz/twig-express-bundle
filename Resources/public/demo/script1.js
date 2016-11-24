a354d975138aabc8e79a6f5fa618f40b3859f89d
Florens Verschelde <florens@fvsch.com>
2016-11-24 15:19:55 +0100

    Remove unused demo assets

--- Resources/public/demo/script1.js
+++
@@ -1,2 +0,0 @@
-document.body.insertAdjacentHTML('beforeend',
-  '<div class="test">Script 1/2 loaded</div>');
--- Resources/public/demo/script2.js
+++
@@ -1,2 +0,0 @@
-document.body.insertAdjacentHTML('beforeend',
-  '<div class="test">Script 2/2 loaded</div>');
